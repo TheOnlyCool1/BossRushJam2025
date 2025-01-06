87165abb94eebc0e12f2cdc88b152e57ed3d8606
papercdisc <papercdisc@gmail.com>
2025-01-05 23:48:26 -0800

    More work on movement script

--- BossRushJam2025/Assets/Scripts/PlayerScripts/MovementStats.cs
+++ BossRushJam2025/Assets/Scripts/PlayerScripts/MovementStats.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-[CreateAssetMenu(fileName = "MovementStats", menuName = "Scriptable Objects/MovementStats")]
+[CreateAssetMenu(fileName = "MovementStats", menuName = "Scriptable Objects/MovementStats", order = 1)]
 public class MovementStats : ScriptableObject
 {
     // Will add specific stats later, will be referenced in player movement script
+
+    public float JumpBuffer;
+
+    public float CoyoteTime;
 }
--- BossRushJam2025/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ BossRushJam2025/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -7,26 +9,108 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody _rb;
     private Collider _col;
+    private FrameInput _fInput;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Keybinds")]
+    public List<KeyCode> JumpKeys = new List<KeyCode>();
+
     void Awake()
     {
         _rb = GetComponent<Rigidbody>();
         _col = GetComponent<Collider>();
     }
 
-    // Update is called once per frame
+    private float _time;
+
     void Update()
     {
-        
+        _time += Time.deltaTime;
+        GetInput();
     }
 
-    // To Do list: 
-    // input handler
+    private void GetInput() // input handler
+    {
+        _fInput = new FrameInput
+        {
+            JumpDown = Input.GetButtonDown("Jump") || AnyKeyDown(JumpKeys.ToArray()),
+            JumpHeld = Input.GetButton("Jump") || AnyKeyHeld(JumpKeys.ToArray()),
+            Move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"))
+        };
+
+        // original script has stuff to accomodate joystick drift/deadzones, axed it cause we aren't planning on adding controller support
+
+        if (_fInput.JumpDown) // works with any of the keys in the jump list
+        {
+            _timeJumpWasPressed = _time;
+            Debug.Log("jump pressed");
+        }
+    }
+
+    #region Handle Key Lists
+    private bool AnyKeyDown(IEnumerable<KeyCode> keys)
+    {
+        foreach (KeyCode key in keys)
+        {
+            if (Input.GetKeyDown(key))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    private bool AnyKeyHeld(IEnumerable<KeyCode> keys)
+    {
+        foreach (KeyCode key in keys)
+        {
+            if (Input.GetKey(key))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    #endregion
+
+    private void FixedUpdate()
+    {
+        // execute jump/movement/etc.
+    }
+
+    //collisions
+    private float _frameLeftGrounded;
+    private bool _grounded;
+
     // jump
+    private bool _jumpToConsume; // "to consume" as in the jump to be executed
+    private bool _queuedJumpUsable;
+    private bool _jumpReleasedEarly; 
+    private bool _coyoteUsable;
+    private float _timeJumpWasPressed;
+
+    private bool HasJumpQueued => _queuedJumpUsable && _time < _timeJumpWasPressed + _moveStats.JumpBuffer; // queues up next jump if the button was pressed within specified time
+    private bool CanUseCoyote => _coyoteUsable && !_grounded && _time < _frameLeftGrounded + _moveStats.CoyoteTime; // allows for player to jump for short period after walking off a ledge
+
+    private void HandleJump()
+    {
+        if(!_jumpReleasedEarly && !_grounded && !_fInput.JumpHeld && _rb.linearVelocity.y > 0) _jumpReleasedEarly = true; // once player has left the ground and the jump key is not being held, _jumpReleasedEarly is true
+
+
+    }
+    private void ExecuteJump()
+    {
+
+    }
+
     // coyote time
     // clamped fall speed
     // edge detection
     // apex modifiers (ie changing speed at the apex of a jump)
     // jump buffering (recording player jump input if player hasn't touched the ground yet)
+}
+
+public struct FrameInput // for storing the player's inputs
+{
+    public bool JumpDown;
+    public bool JumpHeld;
+    public Vector2 Move;
 }
